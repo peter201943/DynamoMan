b465fb1d9c469ac8c9dee9672d97b93cc68a51dc
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-14 15:33:13 -0400

    remove unused lines from camera script

--- Assets/Script/CameraLookCharacter.cs
+++ Assets/Script/CameraLookCharacter.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class CameraLookCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Player;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         this.gameObject.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, this.gameObject.transform.position.z);
-
     }
 }
