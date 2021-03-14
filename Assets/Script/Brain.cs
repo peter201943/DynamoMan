a26b7e0cdc254ab7882b714b33681a0543b2b146
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-14 17:30:24 -0400

    Add a power health class and a brain interface class

---
+++ Assets/Script/Brain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A generic entity script that others extend
+/// <see href="https://forum.unity.com/threads/c-interfaces-tutorial.159849/">C# interfaces tutorial</see>
+/// </summary>
+public interface Brain
+{
+    /// <summary>
+    /// Kill this entity
+    /// Stops it from moving
+    /// Starts whatever death means for this thing
+    /// </summary>
+    public void Die();
+
+    /// <summary>
+    /// Tell this entity to start moving around
+    /// </summary>
+    public void Spawn();
+}
---
+++ Assets/Script/Power.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Power:
+/// * A class, similar to Health in other videogames
+/// * Damage
+/// * Heal
+/// * Current
+/// * Max
+/// </summary>
+[RequireComponent(typeof(Brain))]
+public class Power : MonoBehaviour
+{
+    [Header("Power Level Settings")]
+    
+    [Tooltip("Max Power an entity can ever have")]
+    [SerializeField]
+    private int max     = 100;
+    
+    [Tooltip("Power an entity has at game start")]
+    [SerializeField]
+    private int start   = 50;
+
+    [Tooltip("Current Power of an entity throughout a game")]
+    [SerializeField]
+    private int current = 50;
+
+    [Tooltip("Power needs some brain to tell to die")]
+    [SerializeField]
+    private Brain brain;
+
+    /// <summary>
+    /// Get the current power level
+    /// </summary>
+    public int Current()
+    {
+        return current;
+    }
+
+    /// <summary>
+    /// Take power away from this entity
+    /// OR
+    /// Give this entity power
+    /// </summary>
+    /// <param name="amount">amount of energy to take away</param>
+    /// <returns>Energy taken from entity</returns>
+    public int Change(int amount)
+    {
+        // Play Sound Effect
+        // TODO
+
+        // Deduct Power
+        int remaining = current + amount;
+        if (remaining <= 0)
+        {
+            remaining = 0;
+            brain.Die();
+        }
+
+        // Return Remaining
+        return remaining;
+    }
+}
