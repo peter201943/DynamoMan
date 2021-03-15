430de24ed5ebea5fe45afdb0845ef9585dba0232
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-15 00:45:08 -0400

    Add player brain skeleton

---
+++ Assets/Script/BrainPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainPlayer : Brain
+{
+    private bool alive;
+
+    private void Start()
+    {
+        alive = true;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Spawn()
+    {
+        alive = true;
+    }
+
+    public void Die()
+    {
+        alive = false;
+    }
+
+    public void Respawn()
+    {
+        Spawn();
+    }
+}
