fb6a314c61175baad64decb6a19270ee11b0fa2e
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-15 02:13:44 -0400

    Add damage to bullet; add self destruct to bullet

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    /// <summary>
+    /// Ensure we do not leave any spare bullets around by accident
+    /// </summary>
+    private void Start()
+    {
+        Destroy(obj: gameObject, t: 2f);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Collides with a like-charged object
         if (collision.gameObject.tag == this.gameObject.tag)
         {   
             // Notify the target (if it is of our charge)
             try
             {
-                // collision.gameObject.GetComponent<Power>().Current -= damage;
+                collision.gameObject.GetComponent<Power>().Current -= damage;
             }
             catch
             {
@@ -22,6 +30,7 @@ public class Bullet : MonoBehaviour
             Destroy(this.gameObject);
         }
 
+        // Colides with a Wall
         if (collision.gameObject.tag != "Player" && collision.gameObject.tag != this.gameObject.tag)
         {
             Destroy(this.gameObject);
