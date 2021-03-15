94e0d0a26a27f15c0bd6ab82dda6a896bcf829c0
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-15 01:23:11 -0400

    Add variance parameter and randomization

--- Assets/Script/SpawnDelayed.cs
+++ Assets/Script/SpawnDelayed.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 /// Spawns an item for a given delay
 /// <see href="https://answers.unity.com/questions/205911/help-with-looping-a-coroutine-c.html">Help with looping a coroutine (c#)</see>
 /// <see href="https://forum.unity.com/threads/invokerepeating-vs-coroutines-performance.497208/">InvokeRepeating VS Coroutines [Performance]</see>
+/// <see href="https://gamedev.stackexchange.com/questions/114264/whats-the-difference-between-unityengine-random-and-system-random">What's the difference between UnityEngine.Random and System.Random?</see>
 /// </summary>
 public class SpawnDelayed : MonoBehaviour
 {
@@ -20,17 +21,28 @@ public class SpawnDelayed : MonoBehaviour
     [Tooltip("Are we actively spawning items?")]
     [SerializeField]
     private bool spawning = true;
-    
+
+    [Tooltip("Degree of variation in spawn times")]
+    [SerializeField]
+    private int timeVariance = 3;
+    private int timeVarianceMin;
+    private int timeVarianceMax;
+
     /// <summary>
+    /// Get the Variance
     /// Start spawning at instantiation
     /// </summary>
     private void Start()
     {
+        timeVarianceMin = -timeVariance / 2;
+        timeVarianceMax =  timeVariance / 2;
+
         StartCoroutine(spawnItemContinuous(timeDelay));
     }
 
     /// <summary>
     /// Wait for a certain time and try spawning
+    /// Will add some variation
     /// </summary>
     /// <param name="timeDelay">How long to wait before spawning something</param>
     /// <returns></returns>
@@ -38,7 +50,9 @@ public class SpawnDelayed : MonoBehaviour
     {
         while (spawning)
         {
-            yield return new WaitForSecondsRealtime(timeDelay);
+            yield return new WaitForSecondsRealtime(
+                timeDelay + Random.Range(min: timeVarianceMin, max: timeVarianceMax)
+            );
             spawnItem();
         }
     }
