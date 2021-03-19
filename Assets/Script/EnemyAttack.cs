4dae1451650986c5e12142f1e64ef7689551a425
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-18 23:48:51 -0400

    Add power pickup, but will not use; editing enemyattack

--- Assets/Script/EnemyAttack.cs
+++ Assets/Script/EnemyAttack.cs
@@ -19,4 +19,32 @@ public class EnemyAttack : MonoBehaviour
     [Tooltip("How much damage to attack the player with")]
     [SerializeField]
     public  int     attackDamage;
+
+    /// <summary>
+    /// Attacks the PlayerAttack's power reserves
+    /// WARN TEMP DEBUG
+    /// Meant as a temporary measure only
+    /// </summary>
+    private void AttackPlayerAttackTemp()
+    {
+        // Check if attacking object is player and we can attack
+        // TODO
+
+        // Effects
+        // TODO
+
+        // LOGIC
+        // TODO
+
+        // Cooldown
+        // TODO
+    }
+
+    /// <summary>
+    /// Eventual way of attacking any object generically
+    /// </summary>
+    private void AttackPower()
+    {
+        // TODO
+    }
 }
---
+++ Assets/Script/PowerPickup.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// PICKS UP pickups
+/// Is NOT a pickup
+/// Put on objects that you want to be able to pickup batteries
+/// NOTICE NOT IN USE DO NOT USE
+/// </summary>
+public class PowerPickup : MonoBehaviour
+{
+    [Header("Power")]
+    
+    [Tooltip("Power Source of Picker-Upper")]
+    [SerializeField]
+    private Power power;
+
+    [Header("Sounds")]
+
+    [Tooltip("Play sounds for picking up objects")]
+    [SerializeField]
+    private AudioSource     pickupSound;
+
+    [Tooltip("Played on item pickup")]
+    [SerializeField]
+    private AudioClip       pickup;
+
+    // Layer Logic
+    private int             supplyLayer;
+
+    private void Start()
+    {
+        supplyLayer = LayerMask.NameToLayer("Supply");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Positive" && collision.gameObject.layer == supplyLayer)
+        {
+            // Logic
+            power.Current += 1; // TODO
+            Destroy(collision.gameObject);
+
+            // Effects
+            pickupSound.clip = pickup;
+            pickupSound.Play();
+            
+            // Stop Evaluating
+            return;
+        }
+
+        if (collision.gameObject.tag == "Negative" && collision.gameObject.layer == supplyLayer)
+        {
+            // Logic
+            power.Current += 1; // TODO
+            Destroy(collision.gameObject);
+
+            // Effects
+            pickupSound.clip = pickup;
+            pickupSound.Play();
+
+            // Stop Evaluating
+            return;
+        }
+    }
+}
