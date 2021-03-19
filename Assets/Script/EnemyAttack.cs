551b4e14d8792297434bdc2ea0ecfcb89bf8d027
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-18 23:55:34 -0400

    adding more variables; timing logic down

--- Assets/Script/EnemyAttack.cs
+++ Assets/Script/EnemyAttack.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class EnemyAttack : MonoBehaviour
 {
+    [Header("Difficulty")]
+
     [Tooltip("Cooldown between each attack")]
     [SerializeField]
     private float   cooldownMax;
@@ -15,29 +17,58 @@ public class EnemyAttack : MonoBehaviour
     [Tooltip("Current cooldown of player -- DO NOT SET")]
     [SerializeField]
     private float   cooldownCurrent;
-    
+
+    [Tooltip("How much cooldown do enemies start with?")]
+    [SerializeField]
+    private float cooldownStart;
+
     [Tooltip("How much damage to attack the player with")]
     [SerializeField]
     public  int     attackDamage;
+
+    [Header("Sounds")]
+
+    [Tooltip("Emit sounds for attacks")]
+    [SerializeField]
+    private AudioSource attackSounds;
+
+    [Tooltip("Emitted on attack")]
+    [SerializeField]
+    private AudioClip attackSound;
+
+    private void Start()
+    {
+        cooldownCurrent = cooldownStart;
+    }
+
+    private void Update()
+    {
+        cooldownCurrent -= Time.deltaTime;
+    }
 
     /// <summary>
     /// Attacks the PlayerAttack's power reserves
     /// WARN TEMP DEBUG
     /// Meant as a temporary measure only
     /// </summary>
-    private void AttackPlayerAttackTemp()
+    /// <param name="victim">Who we attack</param>
+    private void AttackPlayerAttackTemp(GameObject victim)
     {
         // Check if attacking object is player and we can attack
-        // TODO
+        if (cooldownCurrent < 0.0 && victim.tag == "Player")
+        {
+            // Get the Player
+            PlayerAttack playerAttack = victim.GetComponent<PlayerAttack>();
 
-        // Effects
-        // TODO
+            // Effects
+            // TODO
 
-        // LOGIC
-        // TODO
+            // Attack with our charge type
+            // TODO
 
-        // Cooldown
-        // TODO
+            // Cooldown
+            // TODO
+        }
     }
 
     /// <summary>
